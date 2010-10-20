e63a28bd700ad81d90e44b3c0d44d8e0eaff07a5
Bradley Spaulding <brad.spaulding@gmail.com>
2010-10-20 00:15:24 -0400

    Now set up for deployment. Also, showing Relevant Titles for ChartInstances.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,22 @@
 set :application, "BillboardChartServer"
-set :repository,  "set your repository location here"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :repository,  "git@github.com:bspaulding/BillboardChartServer.git"
+set :scm, :git
+set :user, "motingo"
+set :use_sudo, false
+set :branch, "master"
+set :deploy_via, :copy
+set :copy_cache, true
+set :deploy_to, "/home-svr4/motingo/html/billboard_chart_server"
 
-role :web, "motingo.com"                          # Your HTTP server, Apache/etc
-role :app, "motingo.com"                          # This may be the same as your `Web` server
-role :db,  "motingo.com", :primary => true 		  # This is where Rails migrations will run
+role :web, "motingo.com"
+role :app, "motingo.com"
+role :db,  "motingo.com", :primary => true
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
